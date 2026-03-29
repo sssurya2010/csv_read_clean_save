c09f286de7100c89b2a8e655daedaced1674a068
Surya Sivakumar <sssurya2010@gmail.com>
2026-03-29 12:28:05 -0700

    FinalCode2

--- readCleanExcel.py
+++ readCleanExcel.py
@@ -12,10 +12,18 @@ Customers_path='/Users/aparya/Downloads/Docs/Customers.csv'
 orders_df=pd.read_csv(Orders_path)
 customers_df=pd.read_csv(Customers_path)
 
+# Join Orders & Customers with CustomerID and get only customers with Orders (inner join with orders)
 customer_orders=customers_df.merge(orders_df,how="inner",on="CustomerID",suffixes=["_cust","_ordr"]).copy()
 
+# Check for TotalAmount more than 0 to eliminate invalid rows
 customer_agg=customer_orders[customer_orders["TotalAmount"]>0].copy()
 
+# Calculate Total Amount for each customer (Name & email)
 customer_final=(customer_orders.groupby(["Name","Email"])["TotalAmount"].sum().to_frame())
 
-customer_final.sort_values(by="TotalAmount",ascending=False).head(20).to_csv("FinalOutput.csv")
+# Get top 20 rows by sorting them by Total Amount
+top_20_customers=customer_final.sort_values(by="TotalAmount",ascending=False).head(20)
+
+# Save it to CSV
+top_20_customers.to_csv("FinalOutput.csv")
+
