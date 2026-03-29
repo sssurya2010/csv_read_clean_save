3f36e1dca261b595e1d9f20853705671be7936f4
Surya Sivakumar <sssurya2010@gmail.com>
2026-03-29 12:16:33 -0700

    finalCode1

--- readCleanExcel.py
+++ readCleanExcel.py
@@ -6,15 +6,11 @@
 
 import pandas as pd
 
-
 Orders_path='/Users/aparya/Downloads/Docs/Orders.csv'
 Customers_path='/Users/aparya/Downloads/Docs/Customers.csv'
 
-
-
 orders_df=pd.read_csv(Orders_path)
 customers_df=pd.read_csv(Customers_path)
-
 
 customer_orders=customers_df.merge(orders_df,how="inner",on="CustomerID",suffixes=["_cust","_ordr"]).copy()
 
@@ -22,5 +18,4 @@ customer_agg=customer_orders[customer_orders["TotalAmount"]>0].copy()
 
 customer_final=(customer_orders.groupby(["Name","Email"])["TotalAmount"].sum().to_frame())
 
-
-print(customer_final.sort_values(by="TotalAmount",ascending=False).head(20))
+customer_final.sort_values(by="TotalAmount",ascending=False).head(20).to_csv("FinalOutput.csv")
